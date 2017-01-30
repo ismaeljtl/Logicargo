693a40e6261defa68bb5a04ca5f3af8b78bd905b
ismaeljtl <ismaelteixeira6@gmail.com>
2017-01-30 16:43:55 -0400

    actualizacion de clientes desde modulo de admin lista

--- resources/views/admin/actualizacionClientes.blade.php
+++ resources/views/admin/actualizacionClientes.blade.php
@@ -12,8 +12,7 @@
 			<div class="form-group row">
 				<label class="col-lg-3 col-form-label">Correo de Usuario</label>
 				<div class="col-lg-9">
-					<input class="form-control" type="email" id="correo" name="correo" value="{{$array['usuario'][0]->user}}">
-					<label class="notify">*no deberías cambiar tu correo electrónico</label>
+					<input readonly="readonly" class="form-control" type="email" id="correo" name="correo" value="{{$array['usuario'][0]->user}}">
 				</div>
 			</div>
             <div class="form-group row">
