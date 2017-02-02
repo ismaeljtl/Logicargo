9742034e34fe89b5884c5b0f9f926e7ca479a884
JonathanDVZ <jodaviza@gmail.com>
2017-02-01 20:29:06 -0400

    Cambios-Jonathan

--- database/scripts/Procedimiento Inserts Vehiculos - Cada centro.sql
+++ database/scripts/Procedimiento Inserts Vehiculos - Cada centro.sql
@@ -7,19 +7,19 @@ BEGIN
 	INSERT INTO `Logicargo`.`Vehiculo`
 	(`marca`,`modelo`,`color`,`placa`,`maxCapPaq`,`minCapPaq`,`anio`,`Centro_Distribucion_id`,`Tipo_Vehiculo_id`,`Estado_Vehiculo_id`)
 	VALUES
+	('Schwinn','411','grey','15DF78S',5,0,2016,x,1,1),
+	('Schwinn','411','grey','15DF78S',5,0,2016,x,1,1),
 	('Schwinn','411','Gris','15DF78S',5,0,2016,x,1,1),
-	('Schwinn','411','Gris','15DF78S',5,0,2016,x,1,1),
-	('Schwinn','411','Gris','15DF78S',5,0,2016,x,1,1),
-	('Schwinn','411','Gris','15DF78S',5,0,2016,x,1,1),
-	('Schwinn','411','Gris','15DF78S',5,0,2016,x,1,1),
-	('Schwinn','411','Gris','15DF78S',5,0,2016,x,1,1),
-	('Schwinn','411','Gris','15DF78S',5,0,2016,x,1,1),
-	('Schwinn','411','Gris','15DF78S',5,0,2016,x,1,1),
-	('Bera','BR200-RR','Azul','15DF78S',30,6,2015,x,2,1),
-	('Bera','BR200-RR','Azul','15DF78S',30,6,2015,x,2,1),
-	('Bera','BR200-RR','Azul','15DF78S',30,6,2015,x,2,1),
-	('Toyota','Machito','Blanca','14FD23A',2000,31,2015,x,3,1),
-	('Toyota','Machito','Blanca','14FD23A',2000,31,2015,x,3,1);
+	('Schwinn','411','grey','15DF78S',5,0,2016,x,1,1),
+	('Schwinn','411','grey','15DF78S',5,0,2016,x,1,1),
+	('Schwinn','411','grey','15DF78S',5,0,2016,x,1,1),
+	('Schwinn','411','grey','15DF78S',5,0,2016,x,1,1),
+	('Schwinn','411','grey','15DF78S',5,0,2016,x,1,1),
+	('Bera','BR200-RR','blue','15DF78S',30,6,2015,x,2,1),
+	('Bera','BR200-RR','blue','15DF78S',30,6,2015,x,2,1),
+	('Bera','BR200-RR','blue','15DF78S',30,6,2015,x,2,1),
+	('Toyota','Machito','white','14FD23A',2000,31,2015,x,3,1),
+	('Toyota','Machito','white','14FD23A',2000,31,2015,x,3,1);
     SET x = x + 1;
   END WHILE;
  END;
