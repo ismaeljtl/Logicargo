760c6b7c102f273ff65532d6beaeee8bc6251c3e
JonathanDVZ <jodaviza@gmail.com>
2017-02-01 23:56:31 -0400

    Cambios-Jonathan

--- database/scripts/Procedimiento Inserts Repartidores - Cada Centro.sql
+++ database/scripts/Procedimiento Inserts Repartidores - Cada Centro.sql
@@ -1,53 +1,30 @@
 DELIMITER $$
-CREATE PROCEDURE `Logicargo`.`InsertJefeRepartidores8`()
+CREATE PROCEDURE `Logicargo`.`InsertRepartidores`()
 BEGIN
   DECLARE x INT DEFAULT 1;
+  DECLARE Jefe_id INT;
 
   WHILE x <= 88 DO
 	INSERT INTO `Logicargo`.`Persona`
 	(`user`,`password`,`nombre`,`segundo_nombre`,`apellido`,`segundo_apellido`,`rol`,`fecha_Nac`,`cedula`,`Ciudad_id`,`remember_token`)
 	VALUES
-	(concat_ws(x,'rep1-','@gmail.com'), '$2y$10$fOOpHnBtMFeO8dP4i5J.VOvS3t9404dUVPfpHh.LOhCOrx5xqJ6zi', 'Jesus', 'Ismael', 'Teixeira', 'Lecca', 'empleado', '1990-11-11', 11111111, x, 'vYwdWftz2xXDGE8XeNIbchTWHEyjI5eRSfO2Nm9CzJzgRWNCYtsvbD3RV2qM'); 
-    INSERT INTO `Logicargo`.`Persona`
-	(`user`,`password`,`nombre`,`segundo_nombre`,`apellido`,`segundo_apellido`,`rol`,`fecha_Nac`,`cedula`,`Ciudad_id`,`remember_token`)
-	VALUES
-	(concat_ws(x,'rep2-','@gmail.com'), '$2y$10$fOOpHnBtMFeO8dP4i5J.VOvS3t9404dUVPfpHh.LOhCOrx5xqJ6zi', 'Jesus', 'Ismael', 'Teixeira', 'Lecca', 'empleado', '1990-11-11', 11111111, x, 'vYwdWftz2xXDGE8XeNIbchTWHEyjI5eRSfO2Nm9CzJzgRWNCYtsvbD3RV2qM'); 
-    INSERT INTO `Logicargo`.`Persona`
-	(`user`,`password`,`nombre`,`segundo_nombre`,`apellido`,`segundo_apellido`,`rol`,`fecha_Nac`,`cedula`,`Ciudad_id`,`remember_token`)
-	VALUES
-	(concat_ws(x,'rep3-','@gmail.com'), '$2y$10$fOOpHnBtMFeO8dP4i5J.VOvS3t9404dUVPfpHh.LOhCOrx5xqJ6zi', 'Jesus', 'Ismael', 'Teixeira', 'Lecca', 'empleado', '1990-11-11', 11111111, x, 'vYwdWftz2xXDGE8XeNIbchTWHEyjI5eRSfO2Nm9CzJzgRWNCYtsvbD3RV2qM'); 
-    INSERT INTO `Logicargo`.`Persona`
-	(`user`,`password`,`nombre`,`segundo_nombre`,`apellido`,`segundo_apellido`,`rol`,`fecha_Nac`,`cedula`,`Ciudad_id`,`remember_token`)
-	VALUES
-	(concat_ws(x,'rep4-','@gmail.com'), '$2y$10$fOOpHnBtMFeO8dP4i5J.VOvS3t9404dUVPfpHh.LOhCOrx5xqJ6zi', 'Jesus', 'Ismael', 'Teixeira', 'Lecca', 'empleado', '1990-11-11', 11111111, x, 'vYwdWftz2xXDGE8XeNIbchTWHEyjI5eRSfO2Nm9CzJzgRWNCYtsvbD3RV2qM'); 
+	(concat_ws(x,'rep1-','@gmail.com'), '$2y$10$fOOpHnBtMFeO8dP4i5J.VOvS3t9404dUVPfpHh.LOhCOrx5xqJ6zi', 'Jesus', 'Ismael', 'Teixeira', 'Lecca', 'empleado', '1990-11-11', 11111111, x, 'vYwdWftz2xXDGE8XeNIbchTWHEyjI5eRSfO2Nm9CzJzgRWNCYtsvbD3RV2qM'),
+	(concat_ws(x,'rep2-','@gmail.com'), '$2y$10$fOOpHnBtMFeO8dP4i5J.VOvS3t9404dUVPfpHh.LOhCOrx5xqJ6zi', 'Jesus', 'Ismael', 'Teixeira', 'Lecca', 'empleado', '1990-11-11', 11111111, x, 'vYwdWftz2xXDGE8XeNIbchTWHEyjI5eRSfO2Nm9CzJzgRWNCYtsvbD3RV2qM'),
+	(concat_ws(x,'rep3-','@gmail.com'), '$2y$10$fOOpHnBtMFeO8dP4i5J.VOvS3t9404dUVPfpHh.LOhCOrx5xqJ6zi', 'Jesus', 'Ismael', 'Teixeira', 'Lecca', 'empleado', '1990-11-11', 11111111, x, 'vYwdWftz2xXDGE8XeNIbchTWHEyjI5eRSfO2Nm9CzJzgRWNCYtsvbD3RV2qM'),
+    (concat_ws(x,'rep4-','@gmail.com'), '$2y$10$fOOpHnBtMFeO8dP4i5J.VOvS3t9404dUVPfpHh.LOhCOrx5xqJ6zi', 'Jesus', 'Ismael', 'Teixeira', 'Lecca', 'empleado', '1990-11-11', 11111111, x, 'vYwdWftz2xXDGE8XeNIbchTWHEyjI5eRSfO2Nm9CzJzgRWNCYtsvbD3RV2qM'); 
+    
+    SET Jefe_id = (select id from `Logicargo`.`Empleado` where Centro_Distribucion_id=x and Tipo_Empleado_id=1);
     
     INSERT INTO `Logicargo`.`Empleado`
 	(`fechaInicio`,`Persona_id`,`Centro_Distribucion_id`,`Tipo_Empleado_id`, `Jefe_id`)
 	VALUES
-	('2011-11-11',(SELECT id FROM `Logicargo`.`Persona` WHERE user=concat_ws(x,'rep1-','@gmail.com')),x,2, 
-		(select id from Empleado where Centro_Distribucion_id=x and Tipo_empleado=1)
-    );
-    INSERT INTO `Logicargo`.`Empleado`
-	(`fechaInicio`,`Persona_id`,`Centro_Distribucion_id`,`Tipo_Empleado_id`, `Jefe_id`)
-	VALUES
-	('2011-11-11',(SELECT id FROM `Logicargo`.`Persona` WHERE user=concat_ws(x,'rep2-','@gmail.com')),x,2, 
-		(select id from Empleado where Centro_Distribucion_id=x and Tipo_empleado=1)
-    );
-    INSERT INTO `Logicargo`.`Empleado`
-	(`fechaInicio`,`Persona_id`,`Centro_Distribucion_id`,`Tipo_Empleado_id`, `Jefe_id`)
-	VALUES
-	('2011-11-11',(SELECT id FROM `Logicargo`.`Persona` WHERE user=concat_ws(x,'rep3-','@gmail.com')),x,2, 
-		(select id from Empleado where Centro_Distribucion_id=x and Tipo_empleado=1)
-    );
-    INSERT INTO `Logicargo`.`Empleado`
-	(`fechaInicio`,`Persona_id`,`Centro_Distribucion_id`,`Tipo_Empleado_id`, `Jefe_id`)
-	VALUES
-	('2011-11-11',(SELECT id FROM `Logicargo`.`Persona` WHERE user=concat_ws(x,'rep4-','@gmail.com')),x,2, 
-		(select id from Empleado where Centro_Distribucion_id=x and Tipo_empleado=1)
-    );
+	('2011-11-11',(SELECT id FROM `Logicargo`.`Persona` WHERE user=concat_ws(x,'rep1-','@gmail.com')),x,2,Jefe_id),
+    ('2011-11-11',(SELECT id FROM `Logicargo`.`Persona` WHERE user=concat_ws(x,'rep2-','@gmail.com')),x,2,Jefe_id),
+	('2011-11-11',(SELECT id FROM `Logicargo`.`Persona` WHERE user=concat_ws(x,'rep3-','@gmail.com')),x,2,Jefe_id),
+	('2011-11-11',(SELECT id FROM `Logicargo`.`Persona` WHERE user=concat_ws(x,'rep4-','@gmail.com')),x,2,Jefe_id);
         
     SET x = x + 1;
   END WHILE;
  END;
  
- CALL `Logicargo`.`InsertJefeRepartidores8`();
+ CALL `Logicargo`.`InsertRepartidores`();
